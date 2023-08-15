f5f0a1d183c8784db9899e461e8a6fffec1be1da
MURILOCAR07 <126526847+MURILOCAR07@users.noreply.github.com>
2023-08-15 10:59:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+/*function setup() {
+  createCanvas(400, 400);
+}
+
+  
+function draw() {
+  background(220);
+}*/
+
+var xb;
+var xb1;
+var yb2;
+var yVoltar;
+var xVoltar;
+var larguraVoltar;
+var alturaVoltar;
+var larguraB;
+var alturaB;
+var suaviaB;
+
+var tela  = 0
+var fonteGeral;
+
+/*function preload(){ 
+    fonteGeral= loadFont("bouncy-black.otf");
+}*/
+  
+
+function setup(){
+  //createCanvas(400,400);
+  xb = 100;
+  yb1 = 150;
+  yb2 = 250;
+  yVoltar = 340;
+  xVolta = 270;
+  larguraB = 200;
+  larguraVoltar = 100;
+}
